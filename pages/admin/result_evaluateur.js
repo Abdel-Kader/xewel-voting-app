06803ad45a5ebb7e20db34a9637b9dc6e63c2cc8
Abdel-Kader <kabdel552@gmail.com>
2021-09-27 21:07:12 +0000

    modifications dues aux retours

--- pages/admin/result_evaluateur.js
+++ pages/admin/result_evaluateur.js
@@ -16,14 +16,12 @@ export default function Result_Evaluateur() {
 
     async function getOrateurs() {
         const response = await fetch("/api/users/result?type=2");
-        console.log(response)
         if(response.ok) {
             if (response.status === 204) {
 
             } else {
                 const data = await response.json();
-                setResult(data.users);
-                console.log('resultssssssss======>', data)
+                setResult(data);
                 let foo = [];
                 let res = []
                 for (var i = 0; i < data.users.length; i++) {
@@ -53,10 +51,6 @@ export default function Result_Evaluateur() {
         <>
             <AdminNav/>
             <div className="flex justify-center">
-                <h2 style={{textAlign: 'center'}}>Nombre total de votants j: {result}</h2>
-                <br/>
-                <br/>
-                <br/>
                 <div style={{
                     width: 900,
                     display: 'flex',
